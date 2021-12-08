eaeb932254989721699cf23f0836f595afd63f5b
saleemullah <saleemullah7654321@gmail.com>
2021-12-08 09:48:31 +0500

    basic file add

---
+++ db_file.py
@@ -0,0 +1,5 @@
+
+
+class DataBaseCls:
+    def insert_data(self,id,name,email,image_path):
+        pass
---
+++ mark_attendence.py
@@ -0,0 +1,5 @@
+
+
+class MarkAttendence:
+    def insert_attendence(self,id,name,current_time,current_date):
+        pass

0a8b30ffc673f83c2852a95ee0c51ebfa788378d
saleemullah <saleemullah7654321@gmail.com>
2021-12-08 10:21:17 +0500

    basic file add

--- db_file.py
+++ db_file.py
@@ -1,5 +1,16 @@
+import pymongo
+from pymongo import MongoClient
+from pymongo import collection
+
 
 
 class DataBaseCls:
+    def __init__(self) -> None:
+        dbData = pymongo.MongoClient("mongodb+srv://talat:mongo@test.wupry.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        dbData.myFirstDatabase.employees.insert_one({"_id": 1, "name": "saleem", "Designation": "python"})
+    
+    
     def insert_data(self,id,name,email,image_path):
         pass
+
+DataBaseCls()
